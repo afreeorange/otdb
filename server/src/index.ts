233229dfd4534f14d5fa64b79d9ad9a9802b026a
Nikhil Anand <mail@nikhil.io>
2023-09-20 19:25:57 -0500

    Add hello page

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import compression from "compression";
 import cors from "cors";
 import express, { Request, Response } from "express";
+import packageJson from "../package.json";
 
 import apiRouter from "./api";
 
@@ -17,7 +18,45 @@ app.use(
 app.use("/api", apiRouter);
 
 app.get(["/api", "/"], (_: Request, res: Response) => {
-  res.send("Welcome to the OTDB API");
+  res.send(`
+  <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+        <title>The OTDB API</title>
+        <style>
+          html,
+          body {
+            align-items: center;
+            display: flex;
+            flex-direction: row;
+            font-family: -apple-system, BlinkMacSystemFont, avenir next, avenir, segoe ui, helvetica neue, helvetica, Cantarell, Ubuntu, roboto, noto, arial, sans-serif;
+            height: 100%;
+            justify-content: center;
+            margin: 0;
+            padding: 0;
+          }
+          p {
+            display: flex;
+          }
+          p small:first-child {
+            flex: 1;
+          }
+        </style>
+      </head>
+      <body>
+        <div>
+          <h1>Hello, I am the OTDB API 👋</h1>
+          <p>
+            <small>v${packageJson.version}</small>
+            <small><a href="https://github.com/afreeorange/otdb" title="Link to this project on Github">Github</small>
+          </p>
+        </div>
+      </body>
+    </html>
+
+  `);
 });
 
 const server = app.listen(port, () => {
