5a57aa5f5ab497946e48d3aba4c33857ddb71136
Nikhil Anand <mail@nikhil.io>
2024-04-29 19:14:29 -0500

    Lol

--- server/src/db/search/gene.ts
+++ server/src/db/search/gene.ts
@@ -4,25 +4,25 @@ import client from "../client";
 
 export const sql = `
 SELECT DISTINCT
-  g.transcript_id,
-  g.gene_symbol,
-  g.gene_title,
-  t.seqname,
-  t."start",
-  t.stop,
-  t.strand,
-  t.total_probes
+	g.transcript_id,
+	g.gene_symbol,
+	g.gene_title,
+	t.seqname,
+	t. "start",
+	t.stop,
+	t.strand,
+	t.total_probes,
+	d.*
 FROM
-  annotations_transcripts_gene_assignments g
-INNER JOIN
-  annotations_transcripts t
-  ON g.transcript_id = t.transcript_id
+	annotations_transcripts_gene_assignments g
+	INNER JOIN annotations_transcripts t ON g.transcript_id = t.transcript_id
+	INNER JOIN data_expression_transcripts d ON d.transcript_id = g.transcript_id
 WHERE
   g.gene_symbol LIKE ?
   OR g.gene_title LIKE ?
 ORDER BY
-  g.gene_symbol,
-  g.transcript_id
+	g.gene_symbol,
+	g.transcript_id
 ;
 `;
 
