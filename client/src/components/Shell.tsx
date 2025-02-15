76be2a083d061d07f938306032520c4778eac677
Nikhil Anand <1223767+afreeorange@users.noreply.github.com>
2025-02-15 12:02:07 -0600

    Lol

--- client/src/components/Shell.tsx
+++ client/src/components/Shell.tsx
@@ -2,20 +2,27 @@ import clsx from "clsx";
 
 import { Footer } from "./Footer";
 import Header from "./Header";
-import { Outlet } from "react-router";
+import { Outlet, useLocation, useParams } from "react-router";
+import { useSearchParams } from "../hooks";
 
-const Shell = ({ padContent = true }: { padContent?: boolean }) => (
-  <>
-    <Header />
-    <div
-      className={clsx({
-        "mx-auto mb-4 mt-8 max-w-4xl px-2": padContent,
-      })}
-    >
-      <Outlet />
-    </div>
-    <Footer className="m-auto max-w-4xl my-16" />
-  </>
-);
+const Shell = ({ padContent = true }: { padContent?: boolean }) => {
+  const loc = useLocation();
+
+  console.log("Shell", loc);
+
+  return (
+    <>
+      <Header />
+      <div
+        className={clsx({
+          "mx-auto mb-4 mt-8 max-w-4xl px-2": padContent,
+        })}
+      >
+        <Outlet />
+      </div>
+      <Footer className="m-auto max-w-4xl my-16" />
+    </>
+  );
+};
 
 export default Shell;
